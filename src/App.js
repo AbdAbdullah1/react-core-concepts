5d40b51ee6795b13742c7088accd579baf64ea8b
Abd Abdullah <abdonlyone@gmail.com>
2021-02-24 14:22:18 +0600

    don

--- src/App.js
+++ src/App.js
@@ -39,10 +39,13 @@ console.log(names);
 
 function  Counter() {
   const [count, setCount] = useState(10);
+  const handleIncrease =() =>setCount(count +1);
+
   return(
     <div>
       <h1>Count: {count} </h1>
-      <button>Increase</button>
+      <button onClick={()=>setCount(count -1)}>decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
     </div> 
   )
 }
